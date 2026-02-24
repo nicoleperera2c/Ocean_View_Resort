ecf4b5cf8744a766167ad828c5010455a0b1121c
Nicole Perera <nikicasendra18@gmail.com>
2026-02-24 15:06:35 +0530

    feat: Implement Singleton DB connection and Custom Exception handlers

---
+++ src/main/java/com/oceanview/exception/ValidationException.java
@@ -0,0 +1,14 @@
+package com.oceanview.exception;
+
+public class ValidationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
